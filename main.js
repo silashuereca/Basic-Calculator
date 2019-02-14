5688ef4340f112880ebd8cdcb96601ce69e9ccc9
silashuereca <silasgranthuereca@gmail.com>
2019-02-14 12:52:46 -0600

    Created HTML / JS code to calculate values

---
+++ main.js
@@ -0,0 +1,21 @@
+function calc() {
+  let a = parseInt(document.querySelector("#value1").value);
+  let b = parseInt(document.querySelector("#value2").value);
+  let op = document.querySelector("#operator").value;
+  var calculate;
+
+  if (op == "add") {
+    calculate = a + b;
+  } else if (op == "min") {
+    calculate = a - b;
+  } else if (op == "div") {
+    calculate = a / b;
+  } else if (op == "mul") {
+    calculate = a * b;
+  } else if (op == "squar") {
+    calculate = Math.pow((a+b), 2);
+  }
+
+  document.querySelector("#result").innerHTML = calculate;
+
+}
